558a4b331fd0e4133da3e28abfc9c1732462e6a2
lucl310 <lucthehuman21@gmail.com>
2017-03-04 11:48:02 -1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 // document.getElementsByTagName('button')
-document.body.addEventListener('mousedown', type)
-//document.body.addEventListener('keydown', type)
+document.body.addEventListener('click', type)
+document.body.addEventListener('keydown', type)
+document.body.addEventListener('keyup', unflash)
 
 function type(e) {
-	console.log(e)
+	console.log(e.key)
+	document.getElementById(e.key).style.backgroundColor = "red";
+	document.getElementById('music').pause = false
+	// 
 // 	if (mouse == down) {document.getElementById("ta").innerHTML = "the mouse is down"
 // }
-	// if (true) {}
+// 	if (true) {}
+}
+function unflash(e) {
+	document.getElementById(e.key).style.backgroundColor = "";
 }
 
